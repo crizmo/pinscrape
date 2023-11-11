7b789eac6f901df07a33584a4e025027367ce2e8
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-11-11 23:44:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Pinterest Scraper
+
+This is a simple web scraper for Pinterest that extracts information about a user's board, including the board title, total number of pins, and URLs of the images in the board.
+
+## Table of Contents
+
+- [Features](#features)
+- [Setup](#setup)
+- [Usage](#usage)
+- [API Endpoints](#api-endpoints)
+- [License](#license)
+
+## Features
+
+- Retrieve board information such as title and total number of pins.
+- Extract URLs of images in a Pinterest board.
+- Save the extracted URLs to a JSON file.
+
+## Setup
+
+1. Clone the repository:
+
+    ```bash
+        git clone https://github.com/kurizu/pinscrape.git
+    ```
+
+2. Install the dependencies:
+
+    ```bash
+        cd pin-scrape
+        npm install
+    ```
+
+## Usage
+1. Run the scraper:
+
+    ```bash
+        npm start
+    ```
+
+2. Open your browser and navigate to http://localhost:3000.
+
+3. Explore the provided API endpoints to get board information and image URLs.
+
+## API Endpoints
+
+### Params
+
+Url takes **two** parameters
+
+- **username** : Pinterest username
+
+- **boardName** : Pinterest board name
+
+### Example
+
+- **URLs**
+	- **home :**  http://localhost:3000/
+	- **Info :**  http://localhost:3000/api/:username/:boardName/info
+	- **Pins :**  http://localhost:3000/api/:username/:boardName/pins
+
+- **To get Info** :
+
+  - Go to `http://localhost:3000/api/kurizu/pookie/info` in your browser
+
+- Result
+
+  ```json
+  {
+    "title": "pookie",
+    "totalPins": 233
+  }
+  ```
+
+- To get pins
+  - Go to `http://localhost:3000/api/kurizu/pookie/pins` in your browser
+
+- **Result**
+
+  ```json
+  {
+    "images": [
+          {
+              "src": "https://i.pinimg.com/280x280_RS/2e/2d/50/2e2d505f82e118adf0ac6e742e503d82.jpg",
+              "alt": "Words"
+          },
+          {
+              "src": "https://i.pinimg.com/564x/2f/8e/1e/2f8e1e44ebac2f3536c991524bfa114f.jpg",
+              "alt": "  The Words, Cool Words, Favorite Quotes, Best Quotes, Love Quotes, Inspirational Quotes, Daily Quotes, Motivational, Pretty Words"
+          },
+          {
+
+          },
+          ...
+    ]
+  }
+
+  ```
---
+++ index.js
@@ -0,0 +1,80 @@
+"use strict";
+
+var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
+var fs = require('fs');
+
+var app = express();
+var url = "https://pinterest.com/";
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.json({
+        message: "Pinterest Scraper By Kurizu",
+        routes: {
+            Info: req.protocol + '://' + req.get('host') + "/api/:username/:boardName/info",
+            Pins: req.protocol + '://' + req.get('host') + "/api/:username/:boardName/pins",
+        },
+    });
+});
+
+app.get("/api/:boardName", function (req, res) {
+    res.json({ BoardName: req.params.boardName });
+});
+
+app.get("/api/:username/:boardName/info", function (req, res) {
+    var title = "";
+
+    request.get(url + req.params.username + '/' + req.params.boardName, function (error, response, html) {
+        var $ = cheerio.load(html);
+
+        title = $("h1").text();
+        var count = parseInt($("header")
+            .find('div > div > div[data-test-id="board-count-info"]')
+            .text()
+            .replace(/\D/g, ""));
+
+        res.json({
+            title: title,
+            totalPins: count,
+        });
+    });
+});
+
+app.get("/api/:username/:boardName/pins", function (req, res) {
+    var results = [];
+    var results_file = [];
+
+    request.get(url + req.params.username + '/' + req.params.boardName, function (error, response, html) {
+        var $ = cheerio.load(html);
+
+        $("img").each(function (i, image) {
+            var re = /236x/gi;
+            var src = $(image).attr("src");
+            var alt = $(image).attr("alt");
+            if (src) {
+                src = src.toString().replace(re, "564x");
+                results[i] = { src: src, alt: alt };
+                results_file.push(src);
+            }
+        });
+
+        // Save the extracted URLs to a JSON file without the "images" property
+        var jsonContent = JSON.stringify(results_file, null, 2);
+        fs.writeFileSync('urls.json', jsonContent, 'utf8');
+
+        res.json({ images: results });
+    });
+});
+
+var port = process.env.PORT || 3000;
+console.log("PORT " + port);
+app.listen(port, function () {
+    console.log("App Listening on PORT " + port);
+});
